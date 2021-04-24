88d8e5335c3dfbf8a0cece8f755cca37486ace1b
themidnightpoet <55615211+themidnightpoet@users.noreply.github.com>
2021-04-24 19:14:20 +0530

    Added other orders

--- mult2.cpp
+++ mult2.cpp
@@ -22,6 +22,60 @@ void multiply(int r,int c,int *v1, int *v2,int *v3)
   }
 }
 
+void multiply2(int r,int c,int *v1, int *v2,int *v3)
+{
+  int i,j,k;
+  int tot = 0;
+  for(j=0;j<c;j++)
+  {
+    for(i=0;i<r;i++)
+    {
+      for(k=0;k<c;k++)
+      {
+        tot = tot + (*((v1+i*c) + k)) * (*((v2+k*c) + j));
+      }
+      *((v3+i*c) + j)=tot;
+      tot=0;
+    }
+  }
+}
+
+void multiply3(int r,int c,int *v1, int *v2,int *v3)
+{
+  int i,j,k;
+  int tot = 0;
+  for(j=0;j<c;j++)
+  {
+    for(k=0;k<r;k++)
+    {
+      for(i=0;i<c;i++)
+      {
+        tot = tot + (*((v1+i*c) + k)) * (*((v2+k*c) + j));
+      }
+      *((v3+i*c) + j)=tot;
+      tot=0;
+    }
+  }
+}
+
+void multiply4(int r,int c,int *v1, int *v2,int *v3)
+{
+  int i,j,k;
+  int tot = 0;
+  for(k=0;k<c;k++)
+  {
+    for(j=0;j<r;j++)
+    {
+      for(i=0;i<c;i++)
+      {
+        tot = tot + (*((v1+i*c) + k)) * (*((v2+k*c) + j));
+      }
+      *((v3+i*c) + j)=tot;
+      tot=0;
+    }
+  }
+}
+
 int main()
 {
   srand((unsigned int)time(NULL));
@@ -88,7 +142,7 @@ for(x=2;x<401;x++)
   }
 
   auto start = high_resolution_clock::now();
-  multiply(x,x,(int *)arr1,(int *)arr2,(int *)arr3);
+  multiply3(x,x,(int *)arr1,(int *)arr2,(int *)arr3);
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<microseconds>(stop - start);
   //cout<<duration.count()<<" microseconds"<<endl;
@@ -159,6 +213,62 @@ return 0;
     cout<<endl;
   }
 */
+/*
+x=3;
+int a1[x][x]=     {
+                      {1,2,3},
+                      {4,5,6},
+                      {7,8,9}
+                    };
+int a2[x][x]= {
+                    {7,8,6},
+                    {5,4,3},
+                    {2,6,5}
+                  };
+int a3[x][x];
 
+for(i=0;i<x;i++)
+{
+  for(j=0;j<x;j++)
+  {
+    a3[i][j] = 0;
+  }
+}
+//generate array
 
+multiply(x,x,(int *)a1,(int *)a2,(int *)a3);
+cout<<"i j k"<<endl;
+for(i=0;i<r;i++)
+{
+  for(j=0;j<c;j++)
+  {
+    cout<<a3[i][j]<<" ";
+  }
+  cout<<endl;
+}
+cout<<endl;
+multiply2(x,x,(int *)a1,(int *)a2,(int *)a3);
+cout<<"j i k"<<endl;
+for(i=0;i<r;i++)
+{
+  for(j=0;j<c;j++)
+  {
+    cout<<a3[i][j]<<" ";
+  }
+  cout<<endl;
+}
+cout<<endl;
+
+multiply3(x,x,(int *)a1,(int *)a2,(int *)a3);
+cout<<"j k i"<<endl;
+for(i=0;i<r;i++)
+{
+  for(j=0;j<c;j++)
+  {
+    cout<<a3[i][j]<<" ";
+  }
+  cout<<endl;
+}
+cout<<endl;
+*/
 }
